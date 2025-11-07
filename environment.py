fd555d02989921185274b0247d9b69c286680cdf
holistudio <daniel.bin.lu@gmail.com>
2025-11-07 16:52:27 -0500

    Update environment.py
    - rename agents to players (keep separate terms for RL agents later)
    - start Food, Spoon, and Player classes

--- environment.py
+++ environment.py
@@ -1,21 +1,33 @@
-class Table (object):
+class Food(object):
+    def __init__(self):
+        pass
+
+class Spoon(object):
+    def __init__(self):
+        pass
+
+class Player(object):
+    def __init__(self):
+        pass
+
+class Table(object):
     def __init__(self, r, n_seats, max_spoon_angle=45, feed_dist= 0.1, include_other_rew=False, max_timesteps=5000):
         self.radius = r
         self.n_seats = n_seats
 
-        agent_offset = 1
+        player_offset = 1
         food_offset = 1
 
-        self.agent_locations = []
-        self.agent_mouths_open = [False for _ in range(n_seats)]
+        self.player_locations = []
+        self.player_mouths_open = [False for _ in range(n_seats)]
         self.food_locations = []
         self.food_need_replenish = [False for _ in range(n_seats)]
         
         theta = 0
         theta_inc = 360/n_seats
         
-        min_spoon_len = agent_offset + r * 2 - food_offset
-        max_spoon_len = min_spoon_len + food_offset + agent_offset
+        min_spoon_len = player_offset + r * 2 - food_offset
+        max_spoon_len = min_spoon_len + food_offset + player_offset
 
         self.max_spoon_angle = max_spoon_angle
         self.min_spoon_angle = -max_spoon_angle
@@ -30,73 +42,73 @@ class Table (object):
             "spoon_theta": (-max_spoon_angle, max_spoon_angle),
             "open_mouth": (False, True),
             "pick_food": (False, True),
-            "drop_food": (False, True) # this is masked from the agent unless they have food on their spoon
+            "drop_food": (False, True) # this is masked from the player unless they have food on their spoon
         }
 
         self.rewards = {}
 
         for n in self.n_seats:
             self.food_locations.append((r - food_offset), theta)
-            self.agent_locations.append((r + agent_offset, theta))
+            self.player_locations.append((r + player_offset, theta))
             key = f'player_{n}'
             self.rewards[key] = 0
             theta += theta_inc 
 
         self.state = {
-            "agent_locations": self.agent_locations,
-            "agent_mouths_open": self.agent_mouths_open,
+            "player_locations": self.player_locations,
+            "player_mouths_open": self.player_mouths_open,
             "food_locations": self.food_locations,
             "spoon_lengths": self.spoon_lengths,
             "spoon_thetas": self.spoon_thetas,
         }
 
-        self.agent_rewards = [0 for _ in range(n_seats)]
+        self.player_rewards = [0 for _ in range(n_seats)]
         self.include_other_rew = include_other_rew
         if include_other_rew:
-            self.state["agent_rewards"] = self.agent_rewards
+            self.state["player_rewards"] = self.player_rewards
 
         self.terminal = False
         self.t = 0
         self.max_timesteps = max_timesteps
         pass
 
-    def get_observation(self, agent):
+    def get_observation(self, player):
         """
-        Get the observation space w.r.t. to a specific agent's perspective.
+        Get the observation space w.r.t. to a specific player's perspective.
         """
         
-        agent_id = int(agent.split('_')[-1])
+        player_id = int(player.split('_')[-1])
         looped = False
 
-        # most observations list the agent's state first before the other agents' states
+        # most observations list the player's state first before the other players' states
         obs_dict = {
-            "agent_locations": [self.state['agent_locations'][agent_id]],
-            "agent_mouths_open": [self.state['agent_mouths_open'][agent_id]],
-            "food_locations": [self.state['food_locations'][agent_id]],
-            "spoon_lengths": [self.state['spoon_lengths'][agent_id]],
-            "spoon_thetas": [self.state['spoon_thetas'][agent_id]]
+            "player_locations": [self.state['player_locations'][player_id]],
+            "player_mouths_open": [self.state['player_mouths_open'][player_id]],
+            "food_locations": [self.state['food_locations'][player_id]],
+            "spoon_lengths": [self.state['spoon_lengths'][player_id]],
+            "spoon_thetas": [self.state['spoon_thetas'][player_id]]
         }
 
         # for when rewards are provided in the observation
-        # ONLY the OTHER agents' rewards are given as part of the observation
+        # ONLY the OTHER players' rewards are given as part of the observation
         if self.include_other_rew:
-            obs_dict["agent_rewards"] = []
+            obs_dict["player_rewards"] = []
 
-        idx = agent_id + 1
+        idx = player_id + 1
         while not looped:
-            obs_dict['agent_locations'].append(self.state['agent_locations'][idx])
-            obs_dict['agent_mouths_open'].append(self.state['agent_mouths_open'][idx])
+            obs_dict['player_locations'].append(self.state['player_locations'][idx])
+            obs_dict['player_mouths_open'].append(self.state['player_mouths_open'][idx])
             obs_dict['food_locations'].append(self.state['food_locations'][idx])
             obs_dict['spoon_lengths'].append(self.state['spoon_lengths'][idx])
             obs_dict['spoon_thetas'].append(self.state['spoon_thetas'][idx])
 
             # for when rewards are provided in the observation
-            # ONLY the OTHER agents' rewards are given as part of the observation
+            # ONLY the OTHER players' rewards are given as part of the observation
             if self.include_other_rew:
-                obs_dict["agent_rewards"].append(self.state["agent_rewards"][idx])
+                obs_dict["player_rewards"].append(self.state["player_rewards"][idx])
 
             idx += 1
-            if idx == agent_id:
+            if idx == player_id:
                 looped = True
             if idx >= self.n_seats:
                 idx = 0
@@ -110,8 +122,8 @@ class Table (object):
     
     def check_fed(self):
         # get all spoon head locations that have food
-        # get locations of all agents with open mouth
-        # compare distance between all spoon heads with food and open mouth agents
+        # get locations of all players with open mouth
+        # compare distance between all spoon heads with food and open mouth players
         # are within the feed distance
-        # return which agents are fed and which s
+        # return which players are fed and which s
         pass
