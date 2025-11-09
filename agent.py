383a4a13318fddf429f824ce226de6084ffe748f
holistudio <daniel.bin.lu@gmail.com>
2025-11-09 11:46:23 -0500

    Update agent.py

---
+++ agent.py
@@ -0,0 +1,5 @@
+# TODO: PickUpAgent picks up food and just waves it around never dropping it
+
+# TODO: GenerousAgent always picks up food and feeds ito the agent across from it
+
+# TODO: TitTatAgent will behave like GenerousAgent only after it gets fed
