46660ad78eb5783b732bc81d5e3ac71df8562b1c
holistudio <daniel.bin.lu@gmail.com>
2025-11-11 21:39:57 -0500

    Update agent.py
    GenerousAgent always picks up food and feeds it to the agent across

--- agent.py
+++ agent.py
@@ -1,4 +1,4 @@
-# TODO: PlayFoodAgent picks up food and just waves it around never dropping it
+# PlayFoodAgent picks up food and just waves it around never dropping it
 class PlayFoodAgent(object):
     def __init__(self, action_space):
         self.timer = 0
@@ -51,5 +51,53 @@ class PlayFoodAgent(object):
     
 
 # TODO: GenerousAgent always picks up food and feeds ito the agent across from it
+
+class GenerousAgent(object):
+    def __init__(self, action_space):
+        self.timer = 0
+        self.min_spoon_len = action_space["spoon_length"][0]
+        self.max_spoon_len = action_space["spoon_length"][1]
+
+        self.action_space = action_space
+        self.current_action = {
+            "spoon_length": (self.min_spoon_len + self.max_spoon_len)/2,
+            "spoon_theta": 0,
+            "open_mouth": False,
+            "pick_food": False,
+            "drop_food": False # this is masked from the player unless they have food on their spoon
+        }
+        pass
+    
+    def step(self, observation):
+        self.current_action = {
+            "spoon_length": self.min_spoon_len,
+            "spoon_theta": 0,
+            "open_mouth": False,
+            "pick_food": False,
+            "drop_food": False # this is masked from the player unless they have food on their spoon
+        }
+
+        # step 1: set spoon_length to min_spoon_length
+        if self.timer == 1:
+            self.current_action["spoon_length"] = self.min_spoon_len
+        
+        # step 2: pick up food
+        if self.timer == 2:
+            self.current_action["pick_food"] = True
+
+        # step 3: set spoon length to max_spoon_length
+        if self.timer == 3:
+            self.current_action["spoon_length"] = self.max_spoon_len
+
+        # step 4: drop food
+        if self.timer == 4:
+            self.current_action["spoon_length"] = self.max_spoon_len
+            self.current_action["drop_food"] = True
+
+        if self.timer < 4:
+            self.timer += 1
+        else:
+            self.timer = 1
+        return self.current_action["spoon_length"], self.current_action["spoon_theta"], self.current_action["open_mouth"], self.current_action["pick_food"], self.current_action["drop_food"]
 
 # TODO: TitTatAgent will behave like GenerousAgent only after it gets fed
